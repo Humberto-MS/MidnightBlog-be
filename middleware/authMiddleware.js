6620b8a5f528b5e54ff397fa7e1a8c058ca59104
Vanessa <147354806+MarNess24@users.noreply.github.com>
2024-12-16 21:35:24 -0600

    Creación de Middleware y Routes (#1)

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,28 @@
+const jwt = require ( 'jsonwebtoken' );
+const asyncHandler = require ( 'express-async-handler' );
+const User = require ( '../models/usersModel' );
+
+const protect = asyncHandler ( async ( req, res, next ) => {
+    let token;
+
+    if ( req.headers.authorization && req.headers.authorization.startsWith( '' ) ) {
+        try {
+            token = req.headers.authorization.split( ' ' )[1];
+            const decoded = jwt.verify( token, process.env.JWT_SECRETO );
+            req.user = await User.findById( decoded.id ).select( 'password' );
+            next();
+        } catch ( error ) {
+            console.log( error );
+            res.status( 401 );
+            throw new Error( 'Unfortunately, you are not authorized.' );
+        }
+    }
+
+    if ( ! token ) {
+        res.status( 401 );             
+        throw new Error( 'Unfortunately, you are not authorized because the token does not exist.' );
+    }
+
+} );
+
+module.exports = { protect };
