dc55e4b1959486aebeaccb7de674166e1102f91a
Humberto-MS <138087864+Humberto-MS@users.noreply.github.com>
2024-12-17 22:32:40 -0600

    Fixes (#5)
    * Se añadió dockerfile
    
    * fixes

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const dotenv = require ( 'dotenv' ).config();
 const { errorHandler } = require ( './middleware/errorMiddleware' );
 const dbConnection = require ( './connections/dbConnection' );
 const port = process.env.PORT || 8000;
+const cors = require ( 'cors' );
 
 dbConnection();
 const app = express();
 
+app.use ( cors() );
 app.use ( express.json() );
 app.use ( express.urlencoded ( { extended: false } ) );
 
@@ -16,5 +18,5 @@ app.use ( '/api/users', require ( './routes/usersRoutes.js' ) );
 app.use ( errorHandler );
 
 app.listen ( port, () => {
-  console.log ( `Server is listening on http://localhost:${port}...` );
+  console.log ( `Server is listening on port: ${port}` );
 });
